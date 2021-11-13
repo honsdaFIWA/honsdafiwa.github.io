16e68053213e429f29cfffcc95516453cce4d907
hons <94107564+honsdaFIWA@users.noreply.github.com>
2021-11-13 14:40:51 +0700

    joe j4

--- audbut.js
+++ audbut.js
@@ -1,9 +1,13 @@
-const soundtracks = ["sweden", "wethands", "miceonven"];
+const soundtracksogg = ["sweden.oga", "wethands.ogg", "miceonven.ogg"];
+const soundtracksmp4 = ["sweden.mp4", "wethands.mp4", "miceonven.mp4"];
+const engst = ["Sweden - C418", "Wet Hands - C418", "Mice on Venus - C418"]
+var nig = document.getElementsByClassName("srcaudogg");
+var ger = document.getElementsByClassName("srcaudmp4");
+var myAudio = document.getElementById("myAudio");
+var cum = document.getElementById("audbut");
+var i = 0;
 
 function aud_play_pause() {
-    var myAudio = document.getElementById("myAudio");
-    var nig = document.getElementsByClassName("srcaud");
-    var cum = document.getElementById("audbut");
     if (myAudio.paused) {
       myAudio.play();
       cum.classList.remove("fa-play")
@@ -15,7 +19,23 @@ function aud_play_pause() {
     }
 };
 
+function aud_next() {
+  if (i === soundtracksogg.length - 1) {
+      i = 0;
+  } else {
+      i++;
+  }
+
+  nig.src = (`./files/sounds/${soundtracksogg[i]}`).toString();
+  ger.src = (`./files/sounds/${soundtracksmp4[i]}`).toString();
+  document.getElementById("audplaying").innerHTML = (`Playing ${engst[i]}`).toString();
+}
+
 myAudio.onended = function() {
     cum.classList.add("fa-play")
     cum.classList.remove("fa-pause")
+    aud_next();
+    setInterval(() => {
+      if (myAudio.paused) myAudio.play();
+    }, 3000);
 };
